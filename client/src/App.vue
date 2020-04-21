d05918d541bc86b59bc7c8e8c2931e876d067a8c
Kevin Mitchell <kevingm31@gmail.com>
2020-04-20 20:15:25 -0500

    Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavComponent/>
     <router-view/>
   </div>
 </template>
 
+<script>
+import NavComponent from './components/partials/NavComponent.vue';
+export default {
+  name: 'app',
+  components: {
+    NavComponent
+  }
+}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ client/src/components/partials/NavComponent.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav id="main-nav">
+    <div class="container">
+      <router-link to="/"><i class="fas fa-dumbbell fa-2x">Gym Buddy</i></router-link>
+      <div class="social">
+        <a href="http://facebook.com" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+        <a href="http://twitter.com" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+        <a href="http://instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+        <a href="http://youtube.com" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>
+      </div>
+      <ul>
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+      </ul>
+         <div class="buttons">
+      <!-- Check that the SDK client is not currently loading before accessing is methods -->
+      <!-- <div v-if="!$auth.loading"> -->
+        <!-- show login when not authenticated -->
+        <!-- <a v-if="!$auth.isAuthenticated" @click="login" class="button"></a> -->
+          <strong>Sign in</strong>
+        <!-- show logout when authenticated -->
+        <!-- <a v-if="$auth.isAuthenticated" @click="logout"  -->
+          <strong class="button">Logout</strong>
+        <!-- <p v-if="$auth.isAuthenticated">Welcome, {{$auth.user.nickname}}!</p> -->
+
+      <!-- </div> -->
+    </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+    methods: {
+      login() {
+        this.$auth.loginWithRedirect();
+      },
+      logout() {
+        this.$auth.logout({
+          returnTo: window.location.origin
+        });
+      }
+    }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+
+#main-nav #logo {
+  display: flex;
+  align-items: flex-start;
+  margin-left: 1rem;
+  text-decoration: none;
+  text-shadow: 2px 2px 4px #000000;
+  color:whitesmoke;
+}
+
+#main-nav ul .navbar-item {
+  text-decoration: none;
+  padding: 0.75rem;
+  text-shadow: 2px 2px 4px #000000;
+  color:whitesmoke;
+}
+
+#main-nav .container {
+  display: grid;
+  grid-template-columns: 6fr 3fr 2fr 2fr;
+  padding: 1rem;
+  align-items: center;
+  background-color: #2074c1;
+}
+
+#main-nav .social {
+  justify-self: center;
+}
+
+#main-nav .social i {
+  color: #777;
+  margin-right: .5rem;
+  text-shadow: 2px 2px 4px #000000;
+}
+
+</style>
+
